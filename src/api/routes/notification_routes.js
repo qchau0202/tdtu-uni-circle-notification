30e94f8f7afe265cda370d425575daef8e834741
iamchouty <90039986+qchau0202@users.noreply.github.com>
2025-12-08 19:56:51 +0700

    feat: removed unused apis

--- src/api/routes/notification_routes.js
+++ src/api/routes/notification_routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const notificationController = require('../controllers/notification_controller');
-const { authenticateJWT } = require('../middlewares/auth_middleware');
 const {
   createNotificationValidator,
   notificationIdValidator,
